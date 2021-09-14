e9baca3d1572ddc744755dd2f310820e2800b240
Mark Kerzner <mark@elephantscale.com>
2021-09-13 22:19:32 -0500

    updated run.sh

--- thread-safety-labs/run.sh
+++ thread-safety-labs/run.sh
@@ -9,7 +9,8 @@ fi
 
 # (optional) add any extra classpath here
 extra_classpath=""
-classpath="target/*:target/dependency-jars/*:$extra_classpath"
+#classpath="target/*:target/dependency-jars/*:$extra_classpath"
+classpath="target/thread-safety-labs-0.0.1-SNAPSHOT.jar:target/*:target/*:$extra_classpath"
 
 
 java -cp "$classpath"  $*
